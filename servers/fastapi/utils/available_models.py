f80c76abeca011a3c9a4d11ef78e00a213377968
Bastian Dupoirieux <bastian.dupoirieux@s-rating-risikosysteme.de>
2025-12-03 13:44:57 +0000

    Added Azure OpenAI to available models

--- servers/fastapi/utils/available_models.py
+++ servers/fastapi/utils/available_models.py
@@ -1,10 +1,17 @@
 from anthropic import AsyncAnthropic
-from openai import AsyncOpenAI
+from openai import AsyncOpenAI, AsyncAzureOpenAI
 from google import genai
 
 
 async def list_available_openai_compatible_models(url: str, api_key: str) -> list[str]:
     client = AsyncOpenAI(api_key=api_key, base_url=url)
+    models = (await client.models.list()).data
+    if models:
+        return list(map(lambda x: x.id, models))
+    return []
+
+async def list_available_azure_openai_models(url: str, api_key: str, api_version:str) -> list[str]:
+    client = AsyncAzureOpenAI(api_key=api_key, base_url=url, api_version=api_version)
     models = (await client.models.list()).data
     if models:
         return list(map(lambda x: x.id, models))
